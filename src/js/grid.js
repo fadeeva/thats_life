ae90de19335fb6ffdd8b3de91897907d870578f7
Katya Fadeeva <katfadmail@gmail.com>
2017-12-03 21:24:34 +0200

    es6 + gitignore

--- src/js/grid.js
+++ src/js/grid.js
@@ -3,27 +3,27 @@
 window.onload = function() { buildGrid(); }
 window.onresize = function() { buildGrid(); }
 
-var theCanvas = document.getElementById('oecumene');
-var Oecumene = theCanvas.getContext("2d");
+const theCanvas = document.getElementById('oecumene');
+const Oecumene = theCanvas.getContext("2d");
 
 function buildGrid() {
-    var screenWidth = window.innerWidth;
-    var screenHeight = window.innerHeight;
+    let screenWidth = window.innerWidth;
+    let screenHeight = window.innerHeight;
     
     theCanvas.width = parseInt((screenWidth - 100) / 20) * 20;
     theCanvas.height = parseInt((screenHeight - theCanvas.offsetTop - 50) / 20) * 20;
     
-    Oecumene.strokeStyle = "#201c1a";
+    Oecumene.strokeStyle = "#1e1c1b";
     Oecumene.lineWidth = 1;
     
-    for(var i = 1; i < parseInt(theCanvas.width / 20); i++) {
+    for(let i = 1; i < parseInt(theCanvas.width / 20); i++) {
         Oecumene.beginPath();
         Oecumene.moveTo(20 * i, 0);
         Oecumene.lineTo(20 * i, theCanvas.height);
         Oecumene.stroke();
     }
     
-    for(var i = 1; i < parseInt(theCanvas.height / 20); i++) {
+    for(let i = 1; i < parseInt(theCanvas.height / 20); i++) {
         Oecumene.beginPath();
         Oecumene.moveTo(0, 20 * i);
         Oecumene.lineTo(theCanvas.width, 20 * i);
