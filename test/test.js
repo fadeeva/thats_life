64c4d90295b03e9f33f6c85f9b3232b56f08783d
Katya Fadeeva <katfadmail@gmail.com>
2018-04-18 23:13:25 +0200

    deleted example testing

--- test/test.js
+++ test/test.js
@@ -1,12 +1,10 @@
 var assert = require('assert');
 var Oecumene = require('../src/js/Oecumene.js').Oecumene;
 
-describe("A calculator", function () {
-    describe("adding 3 and 4 together", function () {
-        it("should return 7", function () {
-            var o = new Oecumene();
-            var result = o.AddNumbers(3, 4);
-            assert.equal(7, result);
+describe("", function () {
+    describe("", function () {
+        it("", function () {
+        
         });
     });
 });
