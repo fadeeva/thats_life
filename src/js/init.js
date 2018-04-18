4c857f8096f0f71f2007054e484aacec49f87337
Katya Fadeeva <katfadmail@gmail.com>
2018-04-18 23:09:19 +0200

    test and doc

---
+++ src/js/init.js
@@ -0,0 +1,8 @@
+/*window.onload = function() { buildGrid(); }
+window.onresize = function() { buildGrid(); }*/
+
+const oecumene = new Oecumene();
+oecumene.buildGrid();
+
+var result = new Oecumene().AddNumbers(3, 4);
+console.log(result)
